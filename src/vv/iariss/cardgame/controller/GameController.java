1234b0bba2a6f2918c77608e26d2aec2500273d1
VallerichVincent <vallerichvincent@outlook.fr>
2020-11-14 19:11:16 +0100

    Init cardGame project

--- src/vv/iariss/cardgame/controller/GameController.java
+++ src/vv/iariss/cardgame/controller/GameController.java
@@ -1,2 +1,128 @@
-package vv.iariss.cardgame.controller;public class GameController {
+package vv.iariss.cardgame.controller;
+
+import vv.iariss.cardgame.model.Deck;
+import vv.iariss.cardgame.model.Player;
+import vv.iariss.cardgame.model.PlayingCard;
+import vv.iariss.cardgame.view.View;
+
+import java.util.ArrayList;
+
+public class GameController {
+    enum GameState{
+        AddingPlayers,
+        CardsDealt,
+        WinnerRevealed
+    }
+
+    private Deck deck;
+    private ArrayList<Player> players;
+    private Player winner;
+    private View view;
+    private GameState gameState;
+
+    public GameController(View view, Deck deck) {
+        this.view = view;
+        this.deck = deck;
+        players = new ArrayList<Player>();
+        gameState = GameState.AddingPlayers;
+        view.setController(this);
+    }
+
+    public void run() {
+        while (true) {
+            switch (gameState) {
+                case AddingPlayers:
+                    view.promptForPlayerName();
+                    break;
+                case CardsDealt:
+                    view.promptForFlip();
+                    break;
+                case WinnerRevealed:
+                    view.promptForNewGame();
+                    break;
+            }
+        }
+    }
+
+    public void addPlayer(String playerName) {
+        if (gameState == GameState.AddingPlayers) {
+            players.add(new Player(playerName));
+            view.showPlayerName(players.size(), playerName);
+        }
+    }
+
+    public void startGame() {
+        if (gameState != GameState.CardsDealt) {
+            deck.shuffle();
+            int playerIndex = 1;
+            for (Player player : players) {
+                player.addCardToHand(deck.removeTopCard());
+                view.showFaceDownCardForPlayer(playerIndex++, player.getName());
+            }
+            gameState = GameState.CardsDealt;
+        }
+    }
+
+    public void flipCard() {
+        int playerIndex = 1;
+        for (Player player: players) {
+            PlayingCard pc = player.getCard(0);
+            pc.flip();
+            view.showCardForPlayer(playerIndex++, player.getName(),
+                    pc.getRank().toString(), pc.getSuit().toString());
+        }
+
+        evaluateWinner();
+        displayWinner();
+        rebuildDeck();
+
+        gameState = GameState.WinnerRevealed;
+    }
+
+    public void evaluateWinner() {
+        Player bestPlayer = null;
+        int bestRank = -1;
+        int bestSuit = -1;
+
+        for (Player player : players) {
+            boolean newBestPlayer = false;
+
+            if (bestPlayer == null) {
+                newBestPlayer = true;
+            }
+
+            else {
+                PlayingCard pc = player.getCard(0);
+                int thisRank = pc.getRank().value();
+                if (thisRank >= bestRank) {
+                    if (thisRank > bestRank) {
+                        newBestPlayer = true;
+                    } else {
+                        if (pc.getSuit().value() > bestSuit) {
+                            newBestPlayer = true;
+                        }
+                    }
+                }
+            }
+
+            if (newBestPlayer) {
+                bestPlayer = player;
+                PlayingCard pc = player.getCard(0);
+                bestRank = pc.getRank().value();
+                bestSuit = pc.getSuit().value();
+            }
+        }
+
+        winner = bestPlayer;
+    }
+
+    public void displayWinner() {
+        view.showWinner(winner.getName());
+    }
+
+    public void rebuildDeck() {
+        for (Player player : players) {
+            deck.returnCardToDeck(player.removeCard());
+        }
+    }
 }
--- src/vv/iariss/cardgame/view/View.java
+++ src/vv/iariss/cardgame/view/View.java
@@ -1,2 +1,52 @@
-package vv.iariss.cardgame.view;public class View {
+package vv.iariss.cardgame.view;
+
+import vv.iariss.cardgame.controller.GameController;
+
+import java.util.Scanner;
+
+public class View {
+    private GameController controller;
+    private Scanner keyboard = new Scanner(System.in);
+
+    public void setController(GameController gc) {
+        this.controller = gc;
+    }
+
+    public void promptForPlayerName() {
+        System.out.println("Enter Player Name : ");
+        String name = keyboard.nextLine();
+        if (name.isEmpty()) {
+            controller.startGame();
+        } else {
+            controller.addPlayer(name);
+        }
+    }
+
+    public void promptForFlip() {
+//        Press enter to reveal cards
+//        nexLine
+//        flipCard
+    }
+
+    public void promptForNewGame() {
+//        Press enter to deal again
+//        nexLine
+//        startgame
+    }
+
+    public void showPlayerName(int playerIndex, String playerName) {
+//    [playerIndex][playerName]
+    }
+
+    public void showFaceDownCardForPlayer(int playerIndex, String playerName) {
+//    [playerName][][]
+    }
+
+    public void showCardForPlayer(int playerIndex, String playerName, String rank, String suit) {
+//    [playerName][rank][suit]
+    }
+
+    public void showWinner(String winnerName) {
+        System.out.println("Winner ! " + winnerName);
+    }
 }
